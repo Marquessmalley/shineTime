9745f8698d03f256e8875ff68ada21981fcbb460
Marquessmalley <marquessmalley@gmail.com>
2021-07-27 22:58:32 -0400

    changes

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const app = express();
 // GET ROUTES
 const indexRoute = require("./routes/index");
 
-// USE STATIC FILES
+// // USE STATIC FILES
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/js", express.static(__dirname + "public/js"));
 app.use("/images", express.static(__dirname + "public/images"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-// SET VIEWS
+// // SET VIEWS
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
@@ -37,6 +37,4 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
-app.listen(3000, (err) => {
-  console.log(err);
-});
+app.listen(3000);
