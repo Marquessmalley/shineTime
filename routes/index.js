8d89e66332206e37783f69d246cb697928ec838a
Marquessmalley <marquessmalley@gmail.com>
2021-07-31 16:50:19 -0400

    Final Update

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const Contact = require("../models/contact");
 
 router.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { contact: new Contact() });
+});
+
+router.post("/", async (req, res) => {
+  const contact = new Contact({
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.text,
+  });
+  try {
+    const newContact = await contact.save();
+    res.render("confirmation", { contact: newContact });
+  } catch {
+    res.send("error");
+  }
 });
 
 module.exports = router;
